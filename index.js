b0cf0b6ae3ac9fdacdb513ffcf8ef8c05ca42f41
saydou sy <saydoumamadousy@gmail.com>
2024-05-07 19:10:47 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+const promptUser = require('./lib/userInput');
+const generateSVG = require('./lib/svgGenerator');
+const fs = require('fs');
 
+async function run() {
+  try {
+    const { text, textColor, shape, shapeColor } = await promptUser();
+    const svg = generateSVG(text, textColor, shape, shapeColor);
+
+    fs.writeFileSync('logo.svg', svg);
+    console.log('Generated logo.svg');
+  } catch (err) {
+    console.error('Error:', err);
+  }
+}
+
+run();
